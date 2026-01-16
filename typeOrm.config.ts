0cecb6684c6e195004b0d4fa519bee79cd8bb668
code-simple <imran1102@gmail.com>
2026-01-16 13:14:11 +0500

    Initial Commit

---
+++ typeOrm.config.ts
@@ -0,0 +1,21 @@
+import { ConfigService } from '@nestjs/config';
+import { config } from 'dotenv';
+import { DataSource } from 'typeorm';
+import { User } from './src/users/entities/user.entity';
+
+const envFilePath = `.env.${process.env.NODE_ENV || 'development'}`;
+config({ path: envFilePath });
+
+const configService = new ConfigService();
+
+export default new DataSource({
+  type: 'postgres',
+  host: configService.getOrThrow<string>('DB_HOST'),
+  port: configService.getOrThrow<number>('DB_PORT'),
+  username: configService.getOrThrow<string>('DB_USERNAME'),
+  password: configService.getOrThrow<string>('DB_PASSWORD'),
+  database: configService.getOrThrow<string>('DB_NAME'),
+  migrations: ['migrations/**'],
+  entities: [User],
+  subscribers: [],
+});
